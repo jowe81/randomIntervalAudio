ca7cc48f94d3d9fd8fb772c2958a01f4da2b1b0f
Johannes Weber <johannes@drweber.de>
2024-10-21 20:19:14 -0700

    Installed dotenv()

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import fs from "fs";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
